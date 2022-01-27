c5f9aba537d23dec6f61a6e7d9d9c54a604d62bd
andersjepsen <hi@andersjepsen.com>
2022-01-27 11:20:46 +0100

    :lock: removed auth from api path

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -6,27 +6,25 @@ import { schema } from "../../graphql/schema";
 
 const cors = Cors();
 
-export default withApiAuthRequired(
-  cors(async function handler(req, res) {
-    const session = getSession(req, res);
+export default cors(async function handler(req, res) {
+  const session = getSession(req, res);
 
-    const apolloServer = new ApolloServer({
-      schema: schema,
-      context: createContext({ user: session?.user }),
-    });
+  const apolloServer = new ApolloServer({
+    schema: schema,
+    context: createContext({ user: session?.user }),
+  });
 
-    const startServer = apolloServer.start();
-    if (req.method === "OPTIONS") {
-      res.end();
-      return false;
-    }
-    await startServer;
+  const startServer = apolloServer.start();
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+  await startServer;
 
-    await apolloServer.createHandler({
-      path: "/api/graphql",
-    })(req, res);
-  })
-);
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+});
 
 export const config = {
   api: {
